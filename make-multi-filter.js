7824ece54c46112de7d6b9148be314bab1d48a48
ADARSH-KODUMURU <91586189+ADARSH-KODUMURU@users.noreply.github.com>
2023-09-19 01:01:58 +0530

    Update make-multi-filter.js

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,48 +1,21 @@
+'use strict';
+
 function MakeMultiFilter(originalArray) {
     let currentArray = originalArray.slice();
-  
+
     function arrayFilterer(filterCriteria, callback) {
-      // Check if filterCriteria is a function, if not, return currentArray
       if (typeof filterCriteria !== 'function') {
         return currentArray;
       }
-      
+
       currentArray = currentArray.filter(filterCriteria);
   
       if (typeof callback === 'function') {
-        callback(currentArray);
+        callback.call(originalArray, currentArray);
       }
-      
+
       return arrayFilterer;
     }
   
     return arrayFilterer;
-}
-
-var arrayFilterer1 = MakeMultiFilter([1, 2, 3]);
-
-arrayFilterer1(function (elem) {
-  return elem !== 2;
-}, function (currentArray) {
-  
-  console.log(this); 
-  console.log(currentArray); 
-});
-
-arrayFilterer1(function (elem) {
-  return elem !== 3; 
-});
-
-var currentArray = arrayFilterer1();
-console.log('currentArray', currentArray); 
-
-function filterTwos(elem) { return elem !== 2; }
-function filterThrees(elem) { return elem !== 3; }
-var arrayFilterer2 = MakeMultiFilter([1, 2, 3]);
-var currentArray2 = arrayFilterer2(filterTwos)(filterThrees)();
-console.log('currentArray2', currentArray2); 
-
-var arrayFilterer3 = MakeMultiFilter([1, 2, 3]);
-var arrayFilterer4 = MakeMultiFilter([4, 5, 6]);
-console.log(arrayFilterer3(filterTwos)()); 
-console.log(arrayFilterer4(filterThrees)()); 
+  }
