d39fa60d4baa5dd38231b50591341816b59cb893
Avinash Mallampati <72144928+avinash2906@users.noreply.github.com>
2023-09-18 15:30:10 -0400

    Updated template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -1,29 +1,12 @@
+'use strict';
 function TemplateProcessor(template) {
-    this.template = template;
-  }
-  
-  TemplateProcessor.prototype.fillIn = function (dictionary) {
-    const regex = /{{(.*?)}}/g;
-    const filledTemplate = this.template.replace(regex, (match, property) => {
-      if (dictionary.hasOwnProperty(property)) {
-        return dictionary[property];
-      } else {
-        return '';
-      }
-    });
-  
-    return filledTemplate;
-  };
+  this.template = template;
+}
 
-var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
-var dateTemplate = new TemplateProcessor(template);
+TemplateProcessor.prototype.fillIn = function (dictionary) {
+  var filledTemplate = this.template.replace(/{{(.*?)}}/g, function (match, property) {
+    return dictionary[property] || '';
+  });
 
-var dictionary = {month: 'July', day: '1', year: '2016'};
-var str = dateTemplate.fillIn(dictionary);
-
-assert(str === 'My favorite month is July but not the day 1 or the year 2016');
-
-var dictionary2 = {day: '1', year: '2016'};
-var str = dateTemplate.fillIn(dictionary2);
-
-assert(str === 'My favorite month is  but not the day 1 or the year 2016');
+  return filledTemplate;
+};
